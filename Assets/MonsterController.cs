e31df71d3b5b8b2b53b488c5e6ae3c745591705c
Kikkawa <sylve.renault@yahoo.fr>
2021-02-19 13:52:37 +0100

    [Features]Monstre qui se déplace de haut en bas

---
+++ Assets/MonsterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    float speed;
+    // might be useless
+    int health;
+    Transform characterTransform;
+    void Start()
+    {
+        characterTransform = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moving(speed);
+    }
+    void moving(float speed)
+    {
+        characterTransform.Translate((Vector3.up * Time.deltaTime) * speed);
+    }
+}
